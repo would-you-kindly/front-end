c04faa8c7f0d1f8649f9cac4f095faae4994134e
ashchelkunov <ashchelkunov@ICS-KK16811>
2020-11-26 15:44:29 +0500

    Настроена маршрутизация по страницам

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { RegComponent } from './reg/reg.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'reg', component: RegComponent},
+  {path: 'auth', component: AuthComponent},
+  {path: 'dashboard', component: DashboardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
