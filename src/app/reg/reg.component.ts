a8399bbd2f17d53a2ba412fc2a702d2277b21027
ashchelkunov <ashchelkunov@ICS-KK16811>
2020-11-26 16:35:47 +0500

    Заполнение полей пользователя на форме регистрации

--- src/app/reg/reg.component.ts
+++ src/app/reg/reg.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+// import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-reg',
   templateUrl: './reg.component.html',
-  styleUrls: ['./reg.component.scss']
+  styleUrls: ['./reg.component.scss'],
 })
 export class RegComponent implements OnInit {
+  name: string
+  login: string
+  email: string
+  password: string
 
-  constructor() { }
+  constructor(/*private _flashMessagesService: FlashMessagesService*/) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  signUp() {
+    const user = {
+      name: this.name,
+      login: this.login,
+      email: this.email,
+      password: this.password,
+    }
+    if (!user.name) {
+      // this._flashMessagesService.show('Enter your name', {
+      //   cssClass: 'alert-danger',
+      //   timeout: 1000,
+      // });
+      return false;
+    }
+    else if (!user.login) {
+      // this._flashMessagesService.show('Enter your login', {
+      //   cssClass: 'alert-danger',
+      //   timeout: 1000,
+      // });
+      return false;
+    }
+    else if (!user.email) {
+      // this._flashMessagesService.show('Enter your email', {
+      //   cssClass: 'alert-danger',
+      //   timeout: 1000,
+      // });
+      return false;
+    }
+    else if (!user.password) {
+      // this._flashMessagesService.show('Enter your password', {
+      //   cssClass: 'alert-danger',
+      //   timeout: 1000,
+      // });
+      return false;
+    }
+
+    console.log(user);
+    return false;
   }
-
 }
